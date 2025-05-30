d4e5e3cb6ba2603455141ca47fd385372d0f4803
leofukuyama <leofukuyama22@gmail.com>
2025-05-30 11:54:53 -0300

    feat: Add Configuração Inicial Streamlit

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import sqlite3
 import streamlit as st
 import pandas as pd
 import matplotlib as plt
 import seaborn as sns
+
+st.title(":muscle: Sistema de Academia",)
+st.write("O sistema é capaz de controlar os dados de **Clientes**, **Instrutores**, **Planos**, **Treinos** e **Exercícios**")
+
+st.subheader(":clipboard: Listar Clientes e Planos:", divider="grey")
+
+st.subheader(":ledger: Treinos e seus Exercícios:", divider="grey")
+
+st.subheader(":dollar: Total Pagamentos e Último Pagamento Cliente:", divider="grey")
+
+st.subheader(":teacher: Quantos Clientes um Instrutor atende: ", divider="grey")
+
+st.subheader(":heavy_plus_sign: Formulário de Cadastro:", divider="grey")
 
