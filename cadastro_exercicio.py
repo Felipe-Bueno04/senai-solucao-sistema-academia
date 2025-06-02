9ad98e2b36c03101b1ba3fb20ff72b5f67d5a756
Felipe Olivato Coimbra <felipe.olicoimbra@gmail.com>
2025-06-02 10:26:10 -0300

    refactor: Correção de repetições de exercícios

--- cadastro_exercicio.py
+++ cadastro_exercicio.py
@@ -11,23 +11,31 @@ def conectar_banco():
 def cadastrar_exercicio_no_treino():
     conn, cursor = conectar_banco()
 
-    with st.form("form_cadastrar_exercicio_no_treino"):
-        df_treinos = pd.read_sql_query("SELECT * FROM treinos", conn)
-        treino = st.selectbox('Selecione o treino: ', df_treinos['id_treino'])
+    df_treinos = pd.read_sql_query("SELECT * FROM treinos", conn)
+    treino = st.selectbox('Selecione o treino: ', df_treinos['id_treino'])
 
-        df_exercicios = pd.read_sql_query("SELECT * FROM exercicios", conn)
-        exercicio = st.selectbox('Selecione o exercício:', df_exercicios['nome_exercicios'])
-        id_exercicio = int(df_exercicios['id_exercicio'].loc[df_exercicios['nome_exercicios'] == exercicio].iloc[0])
+    treino_exercicios = pd.read_sql_query("SELECT fk_exercicio_id FROM treino_exercicios WHERE fk_treino_id = ?", conn, params=(treino,))
+    exercicios_repetidos = list(treino_exercicios['fk_exercicio_id'])
+    prm_list = ", ".join("?" for _ in exercicios_repetidos)
+    sql_string = f"SELECT * FROM exercicios WHERE id_exercicio NOT IN({prm_list})"
+    df_exercicios = pd.read_sql_query(sql_string, conn, params=(exercicios_repetidos))
+    if df_exercicios['id_exercicio'].shape[0] != 0:
+        with st.form("form_cadastrar_exercicio_no_treino"):
+            
+            exercicio = st.selectbox('Selecione o exercício:', df_exercicios['nome_exercicios'])
+            id_exercicio = int(df_exercicios['id_exercicio'].loc[df_exercicios['nome_exercicios'] == exercicio].iloc[0])
 
-        series = int(st.number_input("Número de séries:", min_value=1, step=1))
-        repeticoes = int(st.number_input("Número de repetições:", min_value=1, step=1))
+            series = int(st.number_input("Número de séries:", min_value=1, step=1))
+            repeticoes = int(st.number_input("Número de repetições:", min_value=1, step=1))
 
-        enviar = st.form_submit_button("Cadastrar")
+            enviar = st.form_submit_button("Cadastrar")
 
-        if enviar:
-            if treino and exercicio and series and repeticoes:
-                cursor.execute("INSERT INTO treino_exercicios (fk_treino_id, fk_exercicio_id, series, repeticoes) VALUES (?, ?, ?, ?)",(treino, id_exercicio, series, repeticoes))
-                conn.commit()
-                st.success(f"Exercício adicionado com sucesso ao treino! {exercicio.capitalize()} - Treino {treino}.")
-                st.rerun()
-                conn.close()
+            if enviar:
+                if treino and exercicio and series and repeticoes:
+                    cursor.execute("INSERT INTO treino_exercicios (fk_treino_id, fk_exercicio_id, series, repeticoes) VALUES (?, ?, ?, ?)",(treino, id_exercicio, series, repeticoes))
+                    conn.commit()
+                    st.success(f"Exercício adicionado com sucesso ao treino! {exercicio.capitalize()} - Treino {treino}.")
+                    st.rerun()
+    else:
+        st.write('Não há exercícios novos')
+    conn.close()
