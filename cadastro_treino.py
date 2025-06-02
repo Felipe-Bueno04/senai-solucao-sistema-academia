6939fede5517d87c8e23b39bdbca6cccd22f1970
Felipe <felypebueno2004@gmail.com>
2025-06-02 09:43:34 -0300

    Cadastro treino conectado ao bd

--- cadastro_treino.py
+++ cadastro_treino.py
@@ -1,2 +1,13 @@
+import sqlite3
+import streamlit as st
+import pandas as pd
+
+def conectar_banco():
+    conn = sqlite3.connect("banco_academia.db", check_same_thread=False)
+    cursor = conn.cursor()
+    
+    return conn, cursor
+
 def cadastrar_treino():
+    
     print()
